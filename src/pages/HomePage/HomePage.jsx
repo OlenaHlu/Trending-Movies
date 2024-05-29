60555d18684a0fa1afa2f48d59bee726c99c0913
OlenaHlu <glushenkova_es@ukr.net>
2024-05-29 20:45:38 +0300

    fix home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,15 +3,12 @@ import { getTrendingMovies } from "../../components/Api/apiData";
 import Loader from "../../components/Loader/Loader";
 import toast from "react-hot-toast";
 
-import { Link, useLocation } from "react-router-dom";
-
 import css from "./HomePage.module.css";
+import MovieList from "../../components/MovieList/MovieList";
 
 const HomePage = () => {
   const [trendMovies, setTrendMovies] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  const location = useLocation();
 
   const notify = () =>
     toast.error("Sorry, something went wrong!", {
@@ -46,19 +43,7 @@ const HomePage = () => {
     <div className={css.homePageContainer}>
       <h1 className={css.homePageTitle}>Trending today</h1>
       {loading && <Loader />}
-      <ul className={css.homePageList}>
-        {trendMovies.map((movie) => (
-          <li key={movie.id}>
-            <Link
-              to={`/movies/${movie.id}`}
-              state={{ from: location }}
-              className={css.homePageLink}
-            >
-              {movie.title}
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <MovieList movies={trendMovies} />
     </div>
   );
 };
