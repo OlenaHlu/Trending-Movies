3a763c9f08db4b076d7d84394882beb8c40cedbb
OlenaHlu <glushenkova_es@ukr.net>
2024-10-20 18:47:05 +0300

    search bar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,22 @@
 import css from "./SearchBar.module.css";
+import { MdMovieEdit } from "react-icons/md";
 
 const SearchBar = ({ onSearch }) => {
   return (
     <form onSubmit={onSearch} className={css.form}>
-      <input
-        type="text"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search movie"
-        name="searchMovie"
-        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-        required
-        className={css.input}
-      ></input>
+      <div className={css.iconPosition}>
+        <MdMovieEdit className={css.icon} />
+        <input
+          type="text"
+          autoComplete="off"
+          autoFocus
+          // placeholder="Search movie"
+          name="searchMovie"
+          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          required
+          className={css.input}
+        ></input>
+      </div>
       <button className={css.searchBtn} type="submit">
         Search
       </button>
