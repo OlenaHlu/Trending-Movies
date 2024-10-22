b31373968e495db88ba2c8927d7fff71b97eb1e9
OlenaHlu <glushenkova_es@ukr.net>
2024-10-22 21:01:14 +0300

    fixed

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
+import { GiReturnArrow } from "react-icons/gi";
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <p>
-        Sorry, page not found! Please go to <Link to="/">home page</Link>
+    <div className={css.notFound}>
+      <p className={css.text}>
+        Sorry, page not found! Please go to{" "}
+        <Link to="/" className={css.link}>
+          home page! <GiReturnArrow />
+        </Link>
       </p>
     </div>
   );
