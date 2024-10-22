672a752183e89ebe08a582df75f3ef602bb54f24
OlenaHlu <glushenkova_es@ukr.net>
2024-10-22 18:46:49 +0300

    review

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Loader from "../Loader/Loader";
 import css from "./MovieReviews.module.css";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const MovieReviews = () => {
   const [reviewData, setReviewData] = useState([]);
@@ -30,11 +31,14 @@ const MovieReviews = () => {
   return (
     <div className={css.reviewContainer}>
       {loading && <Loader />}
-      <ul>
+      <ul className={css.reviewlist}>
         {reviewData.length > 0 ? (
           reviewData.map(({ id, author, content }) => (
             <li className={css.reviewItem} key={id}>
-              <p className={css.reviewAuthor}>{author}</p>
+              <h3 className={css.reviewAuthor}>
+                {author}
+                <AiOutlineMessage className={css.icon} />
+              </h3>
               <p className={css.reviewContent}>{content}</p>
             </li>
           ))
